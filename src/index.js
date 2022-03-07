2f44a5a2d2f0929977ef29fee132d14f42987316
Carlos Eduardo Medim <kadumedim123@gmail.com>
2022-03-07 18:16:10 -0300

    React-Fragment

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import "./index.css";
 
 import Primeiro from "./components/basicos/Primeiro";
 import ComParametro from "./components/basicos/ComParametro";
+import Fragmento from "./components/basicos/Fragmento";
 
 ReactDOM.render(
   <div id="app">
@@ -11,6 +12,7 @@ ReactDOM.render(
       titulo="Situação do Aluno"
       aluno="carlos"
       nota="9.3" />
+      <Fragmento/>
   </div>,
   document.getElementById("root")
 );
