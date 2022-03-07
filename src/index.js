a675d3564ae25ef05bdc7767aa8d507e3f175230
Carlos Eduardo Medim <kadumedim123@gmail.com>
2022-03-07 17:24:45 -0300

    Ifs Ternários/Props/Markdown

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
-import ReactDOM from 'react-dom';
-import './index.css';
+import ReactDOM from "react-dom";
+import "./index.css";
 
-import Primeiro from './components/basicos/Primeiro.js';
+import Primeiro from "./components/basicos/Primeiro";
+import ComParametro from "./components/basicos/ComParametro";
 
 ReactDOM.render(
-    <div>
-        <Primeiro></Primeiro>
-    </div>,
-    document.getElementById('root'));
+  <div id="app">
+    <Primeiro></Primeiro>
+    <ComParametro
+      titulo="Situação do Aluno"
+      aluno="carlos"
+      nota="9.3" />
+  </div>,
+  document.getElementById("root")
+);
